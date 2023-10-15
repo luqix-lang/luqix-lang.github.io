d98911037354ebe6664e5c00c7f4614c5f8c7358
rogers <variablecode404@gmail.com>
2023-10-15 09:57:29 +0300

    first commit

--- js/main.js
+++ js/main.js
@@ -10,12 +10,12 @@ let nav = document.getElementById('nav');
     foot = document.getElementById('footer');
 
 nav.innerHTML = `
-<div class="col-8 col-10-md px-3-md  flex-wrap flex justify-between align-items-center z-10">
-    <div class="flex py-3 align-items-center justify-start col-10-md">
-        <p id="bar" class="px-2 mr-2 d-none-xxl d-block-md"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2.0" stroke="currentColor" class="w-6 h-6">
+<div class="col-8 col-10-md px-3-md flex-wrap flex justify-between align-items-center z-10">
+    <div class="flex py-2 align-items-center justify-between col-10-md">
+        <a href="index.html"><p class="text-xl f5 logo flex flex-nowrap align-items-center "><img src="https://www.svgrepo.com/show/434351/airbaloon.svg" class="img w-10"/> Luqix</p></a>
+        <p id="bar" class="px-2 d-none-xxl d-block-md"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2.0" stroke="currentColor" class="w-6 h-6">
           <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
         </svg></p>
-        <a href="index.html"><p class="text-xl f5 logo flex flex-nowrap align-items-center "><img src="https://www.svgrepo.com/show/434351/airbaloon.svg" class="img w-10"/> &nbsp; Luqix</p></a>
     </div>
     <div class="flex-xxl d-none-md flex-wrap align-items-center justify-center">
         <a href="docs/docs.html"><p class="px-4 fm flex flex-nowrap align-item-center nav-on">Documentation
@@ -42,7 +42,7 @@ nav.innerHTML = `
         <div class="back" id="close-menu"></div>
         <div class="flex relative justify-start bg-white col-8 h-100">
             <div class="col-10 pt-2">
-                <a href="index.html"><p class="txt text-xl py-3 bold px-2 mx-3 logo">Luqix</p></a>
+                <a href="index.html"><p class="txt text-xl py-3 font-bold px-2 mx-3 logo">Luqix</p></a>
                 <a href="docs/docs.html"><p class="py-2 px-2 mx-3">Document</p></a>
                 <a href="docs/docs.html"><p class="py-2 px-2 mx-3">Tutorials</p></a>
                 <a href="blog.html"><p class="py-2 px-2 mx-3">Blog</p></a>
@@ -58,11 +58,11 @@ nav.innerHTML = `
 
 footer.innerHTML = `
 <footer class="flex flex-wrap col-8 col-8-lg col-9-md col-10-sm px-4-sm justify-around">
-    <div class="col-2-xxl col-5-md py-10-xxl py-5-md">
-        <p class="pacific text-xl">Variable Code</p>
+    <div class="col-2-xxl col-5-md col-10-sm py-10-xxl py-5-md py-2-sm">
+        <p class="font-bold c-logo text-xl">Variable Code</p>
         <p class="arvo">With genuine software</p>
     </div>
-    <div class="col-2-xxl col-5-md py-10-xxl py-5-md">
+    <div class="col-2-xxl col-5-md col-10-sm py-10-xxl py-5-md py-2-sm">
         <p class="font-bold mb-1">On Website</p>
         <a href="docs/docs.html"><p class="py-1">Documentation</p></a>
         <a href=""><p class="py-1">Tutorials</p></a>
@@ -70,7 +70,7 @@ footer.innerHTML = `
         <a href=""><p class="py-1">Support</p></a>
         <a href="docs/install.html"><p class="py-1">Install</p></a>
     </div>
-    <div class="col-2-xxl col-5-md py-10-xxl py-5-md">
+    <div class="col-2-xxl col-5-md col-10-sm py-10-xxl py-5-md py-2-sm">
         <p class="font-bold mb-1">Learn</p>
         <a href="docs/docs.html"><p class="py-1">Documentation</p></a>
         <a href=""><p class="py-1">Tutorials</p></a>
@@ -78,7 +78,7 @@ footer.innerHTML = `
         <a href=""><p class="py-1">Stackoverflow</p></a>
         <a href="https://github.com/luqix-lang/luqix"><p class="py-1">Github</p></a>
     </div>
-    <div class="col-2-xxl col-5-md py-10-xxl py-5-md">
+    <div class="col-2-xxl col-5-md col-10-sm py-10-xxl py-5-md py-2-sm">
         <p class="font-bold mb-1">Details</p>
         <a href=""><p class="py-1">GNU License</p></a>
         <div class="mt-1t">
@@ -89,7 +89,7 @@ footer.innerHTML = `
     </div>
 
 </footer>
-<p class="text-center col-10 opacity-80">@copyright Sept 2023</p>
+<p class="text-center col-10 opacity-80">@copyright Oct 2023</p>
 `
 
 
