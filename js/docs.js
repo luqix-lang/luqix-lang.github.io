6a048e30efb31c2881fc968d33b0456bcaf81985
rogers <variablecode404@gmail.com>
2023-10-30 22:51:34 +0300

    first commit

--- js/docs.js
+++ js/docs.js
@@ -3,17 +3,20 @@ let nav = document.getElementById('nav');
 nav.innerHTML = `
 <div class="col-8 col-10-md px-3-md flex-wrap flex justify-between align-items-center z-10">
     <div class="flex py-2 align-items-center justify-between col-10-md">
-        <a href="../index.html"><p class="text-xl f5 logo flex flex-nowrap align-items-center "><img src="https://www.svgrepo.com/show/434351/airbaloon.svg" class="img w-10"/> &nbsp; Luqix</p></a>
+        <a href="../index.html"><p class="text-xl-4 text-xl-sm f5 logo mr-3 flex flex-nowrap align-items-center ">Luqix</p></a>
         <p id="bar" class="px-2 d-none-xxl d-block-md"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2.0" stroke="currentColor" class="w-6 h-6">
           <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
         </svg></p>
-    </div>
-    <div class="flex-xxl d-none-md flex-wrap align-items-center justify-center">
-        <a href="docs.html"><p class="px-4 fm flex flex-nowrap align-item-center nav-on">Documentation</p></a>
-        <a href=""><p class="px-4 fm flex flex-nowrap align-items-center">Tutorials</p></a>
-        <a href="../blog.html"><p class="px-4 fm flex align-items-center flex-nowrap">Blog</p></a>
-        <a href=""><p class="px-4 fm flex align-items-center flex-nowrap">Support</p></a>
-        <a href=""><p class="px-4 fm flex flex-nowrap align-items-center">Forum &nbsp; 
+
+        <a href="docs.html" class="d-none-md"><p class="px-5 des py-2 flex flex-nowrap align-item-center nav-on">Docs
+        </p></a>
+        <a href="" class="d-none-md"><p class="px-5 des py-2 flex flex-nowrap align-items-center">Tutorials
+        </p></a>
+        <a href="../blog.html" class="d-none-md"><p class="px-5 des py-2 flex align-items-center flex-nowrap">Blog
+        </p></a>
+        <a href="" class="d-none-md"><p class="px-5 des py-2 flex align-items-center flex-nowrap">Install
+        </p></a>
+        <a href="" class="d-none-md"><p class="px-5 des py-2 flex flex-nowrap align-items-center">Forum &nbsp; 
             <svg fill="none" stroke="currentColor" stroke-width="1.5" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" width="21px" height="21px">
                   <path stroke-linecap="round" stroke-linejoin="round" d="M20.25 8.511c.884.284 1.5 1.128 1.5 2.097v4.286c0 1.136-.847 2.1-1.98 2.193-.34.027-.68.052-1.02.072v3.091l-3-3c-1.354 0-2.694-.055-4.02-.163a2.115 2.115 0 01-.825-.242m9.345-8.334a2.126 2.126 0 00-.476-.095 48.64 48.64 0 00-8.048 0c-1.131.094-1.976 1.057-1.976 2.192v4.286c0 .837.46 1.58 1.155 1.951m9.345-8.334V6.637c0-1.621-1.152-3.026-2.76-3.235A48.455 48.455 0 0011.25 3c-2.115 0-4.198.137-6.24.402-1.608.209-2.76 1.614-2.76 3.235v6.226c0 1.621 1.152 3.026 2.76 3.235.577.075 1.157.14 1.74.194V21l4.155-4.155"></path>
                 </svg>
@@ -86,7 +89,7 @@ side.innerHTML = `
          <p class=" mt-3  cat">b. System APIs</p>
          <a href="os.html"><p class="py-1">Os</p></a>
          <a href="time.html"><p class="py-1">Time</p></a>
-         <a href=""><p class="py-1">Threads</p></a>
+         <a href="thread.html"><p class="py-1">Threads</p></a>
          <a href=""><p class="py-1">Parallelism</p></a>
          <a href=""><p class="py-1">Process</p></a>
 
@@ -125,9 +128,9 @@ let navd = document.getElementById('nav-doc');
 
 navd.innerHTML = `
 <div class="flex justify-between align-items-center px-4 z-90 color">
-     <a href="../index.html"><p class="logo">Luqix <sub class="sub">.0.1</sub></p></a>
+     <a href="../index.html"><p class="logo text-xl">Luqix</p></a>
      <p class="text-md">${ID.innerText.toUpperCase()}</p>
-     <p class="px-2" id="barz">
+     <p class="px-2 py-2" id="barz">
          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2.0" stroke="currentColor" class="w-6 h-6">
           <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
         </svg>
--- js/main.js
+++ js/main.js
@@ -10,23 +10,22 @@ let nav = document.getElementById('nav');
     foot = document.getElementById('footer');
 
 nav.innerHTML = `
-<div class="col-8 col-10-md px-3-md flex-wrap flex justify-between align-items-center z-10">
+<div class="col-9 col-10-md px-3-md flex-wrap flex justify-between align-items-center z-10">
     <div class="flex py-2 align-items-center justify-between col-10-md">
-        <a href="index.html"><p class="text-xl f5 logo flex flex-nowrap align-items-center "><img src="https://www.svgrepo.com/show/434351/airbaloon.svg" class="img w-10"/> Luqix</p></a>
+        <a href="index.html"><p class="text-xl-4 text-xl-sm f5 logo mr-3 flex flex-nowrap align-items-center ">Luqix</p></a>
         <p id="bar" class="px-2 d-none-xxl d-block-md"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2.0" stroke="currentColor" class="w-6 h-6">
           <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
         </svg></p>
-    </div>
-    <div class="flex-xxl d-none-md flex-wrap align-items-center justify-center">
-        <a href="docs/docs.html"><p class="px-4 fm flex flex-nowrap align-item-center nav-on">Documentation
+
+        <a href="docs/docs.html" class="d-none-md"><p class="px-5 des py-2 flex flex-nowrap align-item-center nav-on caret">Docs
         </p></a>
-        <a href=""><p class="px-4 fm flex flex-nowrap align-items-center">Tutorials
+        <a href="" class="d-none-md"><p class="px-5 des py-2 flex flex-nowrap align-items-center caret">Tutorials
         </p></a>
-        <a href="blog.html"><p class="px-4 fm flex align-items-center flex-nowrap">Blog
+        <a href="blog.html" class="d-none-md"><p class="px-5 des py-2 flex align-items-center flex-nowrap caret">Blog
         </p></a>
-        <a href=""><p class="px-4 fm flex align-items-center flex-nowrap">Support
+        <a href="" class="d-none-md"><p class="px-5 des py-2 flex align-items-center flex-nowrap caret">Install
         </p></a>
-        <a href=""><p class="px-4 fm flex flex-nowrap align-items-center">Forum &nbsp; 
+        <a href="" class="d-none-md"><p class="px-5 des py-2 flex flex-nowrap align-items-center">Forum &nbsp; 
             <svg fill="none" stroke="currentColor" stroke-width="1.5" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" width="21px" height="21px">
                   <path stroke-linecap="round" stroke-linejoin="round" d="M20.25 8.511c.884.284 1.5 1.128 1.5 2.097v4.286c0 1.136-.847 2.1-1.98 2.193-.34.027-.68.052-1.02.072v3.091l-3-3c-1.354 0-2.694-.055-4.02-.163a2.115 2.115 0 01-.825-.242m9.345-8.334a2.126 2.126 0 00-.476-.095 48.64 48.64 0 00-8.048 0c-1.131.094-1.976 1.057-1.976 2.192v4.286c0 .837.46 1.58 1.155 1.951m9.345-8.334V6.637c0-1.621-1.152-3.026-2.76-3.235A48.455 48.455 0 0011.25 3c-2.115 0-4.198.137-6.24.402-1.608.209-2.76 1.614-2.76 3.235v6.226c0 1.621 1.152 3.026 2.76 3.235.577.075 1.157.14 1.74.194V21l4.155-4.155"></path>
                 </svg>
@@ -110,3 +109,12 @@ close_menu.addEventListener('click', ()=>{
     menu.classList.add('d-none-xxl');
 })
 
+let caret = document.getElementsByClassName('caret');
+
+for(let i = 0; i< caret.length; i++){
+    caret[i].innerHTML += `
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" height="15px" width='20px'>
+          <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 8.25l-7.5 7.5-7.5-7.5" />
+        </svg>
+    `
+}
